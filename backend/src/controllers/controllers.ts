5f1be174686ef2ef3c2caf1573199e02e4a6411f
mwongess <amosmwongelah@gmail.com>
2023-03-13 16:32:49 +0300

    Add new contact, get all contacts and get one specific contact

--- backend/src/controllers/controllers.ts
+++ backend/src/controllers/controllers.ts
@@ -1,35 +1,58 @@
 import { FastifyRequest, FastifyReply } from "fastify";
+import { db } from "../database/db";
 
 export const newContact = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
-  const { name, phone, email } = <
-    { name: string; phone: number; email: string }
-  >request.body;
-  reply.send(request.body);
+  try {
+    const { name, phone, email } = <{ name: string; phone: number; email: string }>request.body;
+    const connection = await db.getConnection();
+    const query = `INSERT INTO contacts (name, phone, email) VALUES  (?, ?, ?) `;
+    await connection.execute(query, [name, phone, email])
+    connection.release()
+    reply.send({message: "Contact added successfully"})
+  } catch (error) {
+    reply.send(error);
+  }
 };
 
 export const getContacts = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
-  reply.send({ contacts: [] });
+  try {
+    const query = `SELECT * FROM CONTACTS`;
+    const connection = await db.getConnection();
+    const [rows]= await connection.query(query);
+    reply.send(rows);
+  } catch (error) {
+    reply.send(error);
+  }
 };
 
 export const getContact = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
+ try {
   const { id } = <{ id: string }>request.params;
-  reply.send(id);
+  const query = `SELECT * FROM contacts WHERE id = ${id}`
+  const connection = await db.getConnection()
+  const [row] = await connection.query(query)
+  reply.send(row)
+ } catch (error) {
+  reply.send(error)
+ }
 };
 
 export const updateContact = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
-  const { id } = <{ id: string }>request.params;
+  try {
+    const { id } = <{ id: string }>request.params;
+  } catch (error) {}
 };
 
 export const deleteContact = async (
