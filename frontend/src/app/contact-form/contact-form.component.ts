b10ee0ed415ac14085746ba06ac168585f72bd94
mwongess <amosmwongelah@gmail.com>
2023-03-20 17:39:24 +0300

    Generate standalone components

---
+++ frontend/src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+
+}
