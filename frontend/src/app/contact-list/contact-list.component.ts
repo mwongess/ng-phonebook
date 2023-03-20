db790fcfa35dc93a7e24e7b4f10c3519daa2399f
mwongess <amosmwongelah@gmail.com>
2023-03-20 17:10:04 +0300

    Add bootstrap css to this app

---
+++ frontend/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent {
+
+}
