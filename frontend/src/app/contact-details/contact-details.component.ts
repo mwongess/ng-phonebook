5c974823336ad3b91ac10d32c7eff5d3b784afaa
mwongess <amosmwongelah@gmail.com>
2023-03-20 17:21:55 +0300

    Generate standalone components

---
+++ frontend/src/app/contact-details/contact-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-contact-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent {
+
+}
