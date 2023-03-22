1ece0f951557098c8f15d6d37d14f34052199148
mwongess <amosmwongelah@gmail.com>
2023-03-22 19:03:03 +0300

    Setup contact list route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,20 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-path: '',
-loadComponent: ()=>
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((h) => h.HomeComponent),
+      children: [
+        {
+          path: '',
+          loadComponent: ()=> import('./contact-list/contact-list.component').then(cl => cl.ContactListComponent)
+        }
+      ]
   },
   {
     path: 'error',
     loadComponent: () =>
       import('./page-not-found/page-not-found.component').then(
         (n) => n.PageNotFoundComponent
-      ),data: {error: '404',message: 'Page cannot be found'}
+      ),
+    data: { error: '404', message: 'Page cannot be found' },
   },
   {
     path: '**',
-    redirectTo: "/error"
-  }
+    redirectTo: '/error',
+  },
 ];
 
 @NgModule({
