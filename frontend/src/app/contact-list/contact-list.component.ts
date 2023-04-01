6e31e6e1caf4e76d1584a47906372357165ce45f
mwongess <amosmwongelah@gmail.com>
2023-04-01 07:57:34 +0300

    Route add/new

--- frontend/src/app/contact-list/contact-list.component.ts
+++ frontend/src/app/contact-list/contact-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css'],
 })
