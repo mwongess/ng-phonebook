4b7f61bfcbdf90e6a1f419d03d37ccc81aa28b7f
mwongess <amosmwongelah@gmail.com>
2023-04-02 14:31:30 +0300

    Update search

--- frontend/src/app/contact-list/contact-list.component.ts
+++ frontend/src/app/contact-list/contact-list.component.ts
@@ -23,6 +23,24 @@ export class ContactListComponent {
     name: 'Mwongela',
     email: "mwongess@gmail.com",
     phone: 254797377943
+  },
+  {
+    id: '1',
+    name: "John Doe",
+    email: "john.doe@example.com",
+    phone: 25434567890,
+  },
+  {
+    id:'2',
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    phone: 25445678901,
+  },
+  {
+    id: '3',
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    phone: 25456789012,
   }];
   constructor() {}
 
--- frontend/src/app/pipes/search.pipe.ts
+++ frontend/src/app/pipes/search.pipe.ts
@@ -2,17 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'search',
-  standalone: true
+  standalone: true,
 })
 export class SearchPipe implements PipeTransform {
-
-  transform(items: any[], searchText: string): any[] {
-    if (!items) return [];
-    if (!searchText) return items;
+  transform(contacts: any[], searchText: string): any[] {
+    if (!contacts) return [];
+    if (!searchText) return contacts;
     searchText = searchText.toLowerCase();
-    return items.filter(item => {
-      return item.name.toLowerCase().startsWith(searchText);
+    return contacts.filter((contact) => {
+      return contact.name.toLowerCase().startsWith(searchText);
     });
   }
-
 }
