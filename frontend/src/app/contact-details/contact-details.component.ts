81ba32b208dcebca7f9b395e28bf3650f2bf5600
mwongess <amosmwongelah@gmail.com>
2023-04-03 14:41:36 +0300

    View independent contact

--- frontend/src/app/contact-details/contact-details.component.ts
+++ frontend/src/app/contact-details/contact-details.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { contact } from '../interfaces/interfaces';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contact-details',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './contact-details.component.html',
-  styleUrls: ['./contact-details.component.css']
+  styleUrls: ['./contact-details.component.css'],
 })
-export class ContactDetailsComponent {
+export class ContactDetailsComponent implements OnInit {
+  contact!: contact
+  id: string | number = ''
+  constructor(private router: Router, private route: ActivatedRoute,private contactService: ContactService) {}
+  ngOnInit(){
+    this.route.params.subscribe(params=>{
+      this.id = params['id']
+
+      this.contact = this.contactService.getContacts().filter(contact => {
+        return contact.id == this.id
+      })[0]
+
+    })
+  }
 
 }
