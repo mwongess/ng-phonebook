a97c63a07d65677418b39cac93ccfb5f4de74a0b
mwongess <amosmwongelah@gmail.com>
2023-04-06 23:42:39 +0300

    Sort contacts alpabetically

--- frontend/src/app/contact-list/contact-list.component.ts
+++ frontend/src/app/contact-list/contact-list.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { SearchPipe } from "../pipes/search.pipe";
+import { SearchPipe } from '../pipes/search.pipe';
 import { FormsModule } from '@angular/forms';
 import { contact } from '../interfaces/interfaces';
 import { ContactService } from '../services/contact.service';
 
 @Component({
-    selector: 'app-contact-list',
-    standalone: true,
-    templateUrl: './contact-list.component.html',
-    styleUrls: ['./contact-list.component.css'],
-    imports: [CommonModule, RouterModule, FormsModule, SearchPipe]
+  selector: 'app-contact-list',
+  standalone: true,
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css'],
+  imports: [CommonModule, RouterModule, FormsModule, SearchPipe],
 })
 export class ContactListComponent implements OnInit {
-  searchText: string = ''
+  searchText: string = '';
   contacts: contact[] = [];
-  constructor(private contactService: ContactService,private router: Router,private route: ActivatedRoute) {}
+  constructor(
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(){
-    this.contacts = this.contactService.getContacts()
+  ngOnInit() {
+    this.contacts = this.contactService
+      .getContacts()
+      .sort((contact_a, contact_b) =>
+        contact_a.firstName > contact_b.firstName ? 1 : -1
+      );
   }
-  seemoreDetails(id: string | number){
-    this.router.navigate([`/contact/details/${id}`], {relativeTo: this.route})
+  seemoreDetails(id: string | number) {
+    this.router.navigate([`/contact/details/${id}`], {
+      relativeTo: this.route,
+    });
   }
-
 }
